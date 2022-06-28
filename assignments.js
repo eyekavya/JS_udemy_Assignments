4ade50d2471cccc306224fa1668be91d04014726
Kavya <kavya@Shivams-MacBook-Pro.local>
2022-06-28 21:34:18 +0530

    assignment completed

---
+++ assignments.js
@@ -0,0 +1,15 @@
+const country = "India";
+const continent = "Asia";
+let population = 1000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+const language = "Hindi";
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof typeof language);
+
+// language = "English";
