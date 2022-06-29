4d4050aab04eba1aa2bf80412ad731e0a33ffbf8
eyekavya <kavyasrivastava7830@gmail.com>
2022-06-29 13:33:14 +0530

    assignment completed

--- assignments.js
+++ assignments.js
@@ -48,3 +48,10 @@ console.log(
   "123" < 57,
   5 + 6 + "4" + 9 - 4 - 2
 );
+
+// const numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
+// if (numNeighbours === 1) console.log("Only 1 border!");
+// else if (numNeighbours > 1) console.log("More than 1 border");
+// else console.log("No borders");
